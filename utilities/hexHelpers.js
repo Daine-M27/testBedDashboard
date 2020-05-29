29fd4d2e381375e089d9806e19246ff549bbfeb4
Daine Marshall <dmarshall@thelightsource.com>
2020-05-29 14:47:28 -0400

    original commit

---
+++ utilities/hexHelpers.js
@@ -0,0 +1,35 @@
+/**
+ * This function returns a string in ascii format from a hex value
+ * @param {string} inputHex 
+ */
+const hexToAscii = inputHex => {
+    let hex = inputHex.toString();
+    let asciiValue = '';
+    
+    for (let i = 0; i < hex.length; i++) {
+        str += String.fromCharCode(parseInt(hex.substr(n,2),16));        
+    }
+    
+    return asciiValue;
+}
+
+
+/**
+ * This function returns a section of a string based on index values
+ * @param {string} inputString 
+ * @param {number} posStart 
+ * @param {number} posEnd 
+ */
+ const rdmHexResponseParse = (inputString, posStart, posEnd) => {
+    let hexString = (inputString.toString()).substring(posStart, posEnd) 
+   
+    return hexString;
+}
+
+
+const _rdmHexResponseParse = rdmHexResponseParse;
+const _hexToAscii = hexToAscii;
+export { 
+    _hexToAscii as hexToAscii,
+    _rdmHexResponseParse as rdmHexResponseParse
+};
