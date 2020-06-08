341f2ce4391d7bd9a6f460bb4b92d84f98fe4781
Daine Marshall <dmarshall@thelightsource.com>
2020-06-08 17:47:32 -0400

    working json parameters

--- utilities/test.js
+++ utilities/test.js
@@ -1,16 +1,33 @@
 const net = require("net");
 const client = new net.Socket();
+const command = {
+    "parameters": {
+        "address": "TCPIP0::192.168.1.170",
+        "command": "MEASure:VOLTage?",
+        "convert": "false"
+    }
+};
 
 client.connect(5001, "127.0.0.1", function(){
     console.log("Connected to C# Socket");
+    let payload = JSON.stringify(command);
     //            (IP address command, SCPI Command, Scientific Notation Convert)
-    client.write("TCPIP0::192.168.1.170,*IDN?,false")
+    //client.write("TCPIP0::192.168.1.170,*IDN?,false")
+    client.write(payload);
+    //client.write("exit")
 })
 
 client.on('data', (data) => {
-    let reading = new TextDecoder().decode(data)
-    
+    let reading = new TextDecoder().decode(data);    
     console.log(reading);
+})
+
+client.on('error', (error) => {
+    console.log(error);
+})
+
+client.on('close', () =>{
+    console.log("connection closed");
 })
 
 //write function to connect and send command with IPaddress parameters to c# SCPI server
