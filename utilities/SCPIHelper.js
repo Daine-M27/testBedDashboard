f7f66fe6c024441358dbe6a6f07635c98bd89328
Daine Marshall <dmarshall@thelightsource.com>
2020-06-11 10:34:12 -0400

    working scpi command class with response

--- utilities/SCPIHelper.js
+++ utilities/SCPIHelper.js
@@ -1,5 +1,5 @@
 const net = require("net");
-const client = new net.Socket();
+
 
 function sendCommand (address, command, convert) { 
     this.address = address;
@@ -14,12 +14,12 @@ function sendCommand (address, command, convert) {
                     "convert": this.convert
                 }
             };            
+            //initialize new socket
+            const client = new net.Socket();
 
             // establish connection
             client.connect(5001, "127.0.0.1", function () {
-
                 console.log("Connected to C# Socket");
-
                 // convert json to pretty string
                 let payload = JSON.stringify(params);
                 console.log("sending payload: " + payload);
@@ -32,6 +32,7 @@ function sendCommand (address, command, convert) {
                 // reading for sending a one way command should be 0 if no errors are detected in sending, otherwise it will be request specific
                 let reading = new TextDecoder().decode(data);
                 this.response = reading;
+                client.destroy();
             });
 
             // close connection
@@ -46,81 +47,12 @@ function sendCommand (address, command, convert) {
             //     this.res = error;
             //     return this.res;
             // });
-
         } catch (error) {
             // log error and return false
             console.log(error)
             //this.res = 1;
             this.response = error;
             //return this.response;
-        }
-        // finally {
-        //     return this.response;
-        // }
-    
-}
-    
+        }         
+}    
 module.exports = { sendCommand: sendCommand };
-
-
-// class InstrumentComm {
-//     constructor(address, command, convert)
-//     {
-//         this.address = address;
-//         this.command = command;
-//         this.convert = convert;
-//         this.params = {
-//             "parameters": {
-//                 "address": this.address,
-//                 "command": this.command,
-//                 "convert": this.convert
-//             }
-//         };
-//         this.res;        
-//     };
-    
-//     sendCommand() {
-//         try {
-//             // establish connection
-//             client.connect(5001, "127.0.0.1", function () {
-//                 console.log("Connected to C# Socket");
-                
-//                 // convert json to pretty string
-//                 let payload = JSON.stringify( this.params );
-//                 console.log("sending payload: " + payload );
-//                 // send payload to destination socket
-//                 client.write(payload);
-                
-//                 // get response
-//                 client.on('data', (data) => {
-//                     // reading for sending a one way command should be 0 if no errors are detected in sending, otherwise it will be request specific
-//                     let reading = new TextDecoder().decode(data);
-//                     this.res = reading;
-//                 });
-                
-//                 // close connection
-//                 client.on('close', () => {
-//                     console.log("connection closed");
-//                     return this.res;
-//                 });
-
-//                 // handle error from SCPI server
-//                 // client.on('error', (error) => {
-//                 //     console.log(error);
-//                 //     this.res = error;
-//                 //     return this.res;
-//                 // });
-//             });
-//         } catch (error) {
-//             // log error and return false
-//             console.log(error)
-//             this.res = 1;
-//             return this.res;
-//         }
-//         finally {
-//             return this.res;
-//         }
-//     };
-// }
-
-// module.exports = InstrumentComm;
--- utilities/test.js
+++ utilities/test.js
@@ -1,32 +1,98 @@
-const net = require("net");
-const client = new net.Socket();
-const params = {
-    "parameters": {
-        "address": "TCPIP0::192.168.1.170",
-        "command": "MEASure:VOLTage?",
-        "convert": "false"
-    }
+const scpi = require("./SCPIHelper");
+
+const queryDeviceID = "*IDN?";
+const queryCurrent = "CURRent?";
+const queryVoltage = "VOLTage?";
+
+const measureVolt = "MEASure:VOLTage?";
+const measureCurrent = "MEASure:CURRent?";
+const measurePower = "MEASure:POWEr?";
+
+const setCurrent = ( ch, val ) => {
+    return ( ch + ":CURRent " + val )
+};
+const setVoltage = ( ch, val ) => {
+    return ( ch + ":VOLTage " + val )
+};
+const setOutput = "OUTPut:TRACK 1";
+
+const onOff = (position) => {
+    return ( "OUTPut CH1,"+position ) 
 };
 
-client.connect(5001, "127.0.0.1", function(){
-    console.log("Connected to C# Socket");
-    let payload = JSON.stringify(params);
+const address = "TCPIP0::192.168.1.170";
+
+///////////////////////////////////////////////////////////////
+
+// const net = require("net");
+// const client = new net.Socket();
+// const params = {
+//     "parameters": {
+//         "address": "TCPIP0::192.168.1.170",
+//         "command":"*IDN?",
+//         "convert": "false"
+//     }
+// };
+
+// const scpiData = () => {
+
+//     client.connect(5001, "127.0.0.1", function(){
+//         console.log("Connected to C# Socket");
+        
+//         //client.write("exit")
+//     })
+
     
-    client.write(payload);
-    //client.write("exit")
-})
 
-client.on('data', (data) => {
-    let reading = new TextDecoder().decode(data);    
-    console.log(reading);
-})
+//     let payload = JSON.stringify(params);
+        
+//     client.write(payload);
 
-client.on('error', (error) => {
-    console.log(error);
-})
+// }
 
-client.on('close', () =>{
-    console.log("connection closed");
-})
+/////////////////////////////////////////////////////////////////
+
+// function testSCPI() {
+//     let response = "";
+// try {
+//     client.connect(5001, "127.0.0.1", function(){
+//         console.log("Connected to C# Socket");
+//         let payload = JSON.stringify(params);
+        
+//         client.write(payload);
+//         //client.write("exit")
+//         client.on('data', (data) => {
+//             let reading = new TextDecoder().decode(data);    
+//             //console.log(reading);
+//             response = reading;
+           
+//             client.destroy();
+//         })
+//     })   
+    
+//     client.on('error', (error) => {
+//         console.log(error);
+//     })
+    
+//     client.on('close', () => {
+//         console.log("connection closed");
+//     })
+
+//     return response;
+
+// } catch (error) {
+//     console.log(error)
+// }
+  
+// }
+
+
+//var answer = testSCPI();
+//module.exports = {testSCPI};
+///////////////////////////////////////////////////////////////////
 
 //write function to connect and send command with IPaddress parameters to c# SCPI server
+
+//"command":"*IDN?",
+//"command": "MEASure:VOLTage?",
+//""
