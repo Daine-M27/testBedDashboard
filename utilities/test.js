8f08a4139cb255fda9f038c5a475e3df54213441
Daine Marshall <dmarshall@thelightsource.com>
2020-06-15 11:46:19 -0400

    working dmx and rdm utility

--- utilities/test.js
+++ utilities/test.js
@@ -1,7 +1,53 @@
+const qs = require('qs');
+const axios = require('axios');
+const green = {
+    1:'128',
+    2:'0',
+    3:'255',
+    4:'0',
+    5:'0',
+    6:'0'
+}
 
+const rdmParams = {
+    command_class:'10',
+    destination:'7151:31323334',
+    pid:'1000',
+    data: '01'
+}
 
+function sendDMX(params){
+    axios({
+        method: 'post',
+        url: 'http://127.0.0.1:5000/v1/dmx',
+        data: qs.stringify(params),
+        headers: {
+            'content-type':'application/x-www-form-urlencoded' 
+        }
+    }).then((res)=>{
+        console.log(res.status);
+    }, (err) => {
+        console.log(err);
+    })
+}
 
+function sendRDM(params){
+    axios({
+        method: 'post',
+        url: 'http://127.0.0.1:5000/v1/rdm',
+        data: qs.stringify(params),
+        headers: {
+            'content-type':'application/x-www-form-urlencoded' 
+        }
+    }).then((res)=>{
+        console.log(res.status);
+    }, (err) => {
+        console.log(err);
+    })
+}
 
+//sendDMX(green);
+//sendRDM(rdmParams);
 
 
 
