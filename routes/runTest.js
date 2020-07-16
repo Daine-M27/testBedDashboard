b75b9cff5da39462fce3917381f7e3b452f2e8d0
Daine Marshall <dmarshall@thelightsource.com>
2020-07-16 14:05:55 -0400

    working runTestPage with instrument check and test selection form

--- routes/runTest.js
+++ routes/runTest.js
@@ -1,32 +1,41 @@
 const express = require('express');
 const scpi = require('../utilities/SCPIHelpers');
+const dbhelper = require('../utilities/databaseHelpers');
 const dotenv = require('dotenv').config({ path: require('find-config')('.env') });
 
 const router = express.Router();
 
-/* GET home page. */
+/* GET test home page. */
 router.get('/', (req, res) => {
-  res.render('.\\runTest\\runTest', { title: 'Run Test Page' });
-});
-
-/* GET data from test instruments */
-router.get('/instrumentCheck', (req, res) => {
   const devices = [
     process.env.DMM_CHAN_0,
-    process.env.DMM_CHAN_0,
-    process.env.DMM_CHAN_0,
-    process.env.DMM_CHAN_0,
+    process.env.DMM_CHAN_1,
+    process.env.DMM_CHAN_2,
+    process.env.DMM_CHAN_3,
+    process.env.PPS,
   ];
 
   scpi.checkInsturments(devices, '*IDN?', 'false')
-    .then((data) => {
-      console.log(data);
-      res.send(data);
+    .then((instrumentCheck) => {
+      // console.log(data);
+      dbhelper.getTestTemplate()
+        .then((testTemplates) => {
+          res.render('.\\runTest\\runTest', { title: 'Run Test Page', instruments: instrumentCheck, templates: testTemplates.recordset });
+        });
     });
 });
 
-/* GET data from test instruments */
-router.get('/startTest', (req, res) => {
+/* GET test specific page */
+router.get('/instrumentCheck', (req, res) => {
+  res.render('.\\runTest\\runTest', { title: 'Test Specific Page' });
+});
+
+/*  */
+router.post('/startTest', (req, res) => {
+  // console.log(req.body.TestTemplateId);
+  
+  // take test template id and redirect to test page
+  res
 
 });
 
